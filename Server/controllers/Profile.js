6aaca5a5971a4f7427b13fd2bd341f2a8a831769
Aman <aman.c9810@gmail.com>
2025-01-26 15:46:03 +0530

    commit

--- Server/controllers/Profile.js
+++ Server/controllers/Profile.js
@@ -1,6 +1,6 @@
 const Profile= require("../models/Profile");
 const User= require("../models/User");
-const { uploadImageToCloudinary } = require("../utils/imageUploader");
+const { uploadImageToCloudinary } = require("../Utils/imageUploader");
 const Course=require("../models/Course");
 
 exports.updateProfile = async (req, res) => {
